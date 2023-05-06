a0ac7457aae92d13ed9183a58cf02ba9758f04c2
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-06 14:07:24 +0200

    Initial Commit

---
+++ PlaceLookupDemo/PlaceLookupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookupDemoApp.swift
+//  PlaceLookupDemo
+//
+//  Created by Eric on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlaceLookupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
